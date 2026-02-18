f2b85ed035cee8c9884a7b5e8e713b5fe175d534
soipanhamisi <soipanhani@gmail.com>
2026-02-18 15:42:36 +0300

    fixes to repo

--- src/main/java/org/soipan/ilas/IlasApplication.java
+++ src/main/java/org/soipan/ilas/IlasApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IlasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IlasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IlasApplication.class, args);
+    }
 
 }
