69181e8ab363d2f0bbfaab789f001755600b2738
soipanhamisi <soipanhani@gmail.com>
2026-02-18 15:36:17 +0300

    Initial commit

---
+++ src/main/java/org/soipan/ilas/IlasApplication.java
@@ -0,0 +1,13 @@
+package org.soipan.ilas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/soipan/ilas/IlasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soipan.ilas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
